3884316b34bc2bbfa041fa1a3aa3003924bea853
Alp <alp@Alps-MacBook-Pro.local>
2017-12-10 18:57:47 +0100

    substituted avconv with pydub, proscript library integration, sub entries are merged

--- src/subsegment_movie.py
+++ src/subsegment_movie.py
@@ -3,14 +3,13 @@ from optparse import OptionParser
 import os
 import sys
 import re
-import subprocess
 import csv
-import cPickle
-import xml.etree.ElementTree as ET
-import credentials
 import speech_recognition as sr
-from nltk.tokenize import RegexpTokenizer
-from datetime import *
+from datetime import datetime, date, time, timedelta
+import pysrt
+from pydub import AudioSegment
+from proscript.proscript import Word, Proscript, Segment
+from proscript.utilities import utils
 
 #CONSTANTS
 TIME_ROUNDUP_FOR_AVCONV = 3
@@ -23,72 +22,7 @@ SENTENCE_END_MARKS = ['.', '?', '!', ':', '...']
 PUNCTUATION_MARKS = [',', ';', '/', '"']
 WAV_BITRATE = 16000
 
-AVCONV_LOC = "avconv"
-
-temptextfile="tmp_subtext.txt"
-tempwavfile="tmp_subaudio.wav"
-tempscribefile="tmp_scribe.xml"
-
-def readSrt(subfile):
-	'''
-	read srt file line by line, each entry (seperated by empty line) has 4 lines:
-	1 - entry id --> stored as id in dict
-	2 - time values --> processed and stored as start and dur in dict
-	3 - subtitle line 1 --> stored as line1 in dict
-	4 - subtitle line 2 --> stored as line2 in dict
-	'''
-	subData = []
-	
-	with open(subfile) as f:
-		lines = f.read().decode("utf-8-sig").encode("utf-8").splitlines()
-		linetype=1		#1-id, 2-timestamps, 3..-text
-				
-		subEntry = {'id':0, 'start':"", 'end':"", 'duration':"", 'textline1':"", 'textline2':"", 'subtext':""}
-		for line in lines:
-			if not line:
-				subEntry['subtext'] = subEntry['subtext'][1:]   #take out the extra whitespace at the beginning
-				subData.append(subEntry)
-				subEntry = {'id':0, 'start':"", 'end':"", 'duration':"", 'textline1':"", 'textline2':"", 'subtext':""}
-				linetype=1
-				continue
-			elif linetype==1:
-				subEntry['id']=line		
-				linetype=linetype+1;
-			elif linetype==2: 
-				timestamp=line
-				 
-				[startstamp, endstamp] = timestamp.split(" --> ")
-				[startstamp, endstamp] = [startstamp.replace(",", ":"), endstamp.replace(",", ":")]
-				[s_hour,s_min,s_sec,s_mil]=startstamp.split(':')
-				[e_hour,e_min,e_sec,e_mil]=endstamp.split(':')
-
-				t_end = time(int(e_hour),int(e_min),int(e_sec),int(e_mil)*1000)
-				dt_end = datetime.combine(date.today(), t_end)
-
-				delta_start = timedelta(hours=int(s_hour), minutes=int(s_min), seconds=int(s_sec), microseconds=int(s_mil)*1000)
-
-				t_dur = (dt_end - delta_start).time()
-				[dur_sec, dur_mil] = [t_dur.second, t_dur.microsecond/1000]
-								
-				subEntry['start'] = "%s:%s:%s.%s"%(s_hour, s_min, s_sec, s_mil)
-				subEntry['end'] = "%s:%s:%s.%s"%(e_hour, e_min, e_sec, e_mil)
-				subEntry['duration'] = "%i.%i"%(dur_sec, dur_mil)
-
-				subEntry['startSecs'] = round(timestamp2secs(startstamp),TIME_ROUNDUP_FOR_AVCONV)
-				subEntry['endSecs'] = round(timestamp2secs(endstamp),TIME_ROUNDUP_FOR_AVCONV)
-				subEntry['durSecs'] = round(subEntry['endSecs'] - subEntry['startSecs'],TIME_ROUNDUP_FOR_AVCONV)
-				linetype=3
-			else:
-				subEntry['subtext'] += ' ' + line
-	return subData
-
-def timestamp2secs(timestamp):
-	#converts timestamp in hh:mm:ss:mmm form to s.mmm
-	[s_hour,s_min,s_sec,s_mil]=timestamp.split(':')
-	seconds=int(s_hour)*60*60 + int(s_min)*60 + int(s_sec)
-	milliseconds=float(s_mil)*0.001
-	return seconds+milliseconds
-
+#TODO: merge to normalize_transcript
 def cleanSrtData(srtData):
 	#preprocesses raw subtitle data
 	for i in reversed(range(len(srtData))):
@@ -122,67 +56,45 @@ def cleanSrtData(srtData):
 			entry['subtext'] = re.sub(r"Uds(\.\s|\s)", "ustedes ", entry['subtext'])
 	return srtData
 
-def checkFile(filename, variable):
-    if not filename:
-        print "%s file not given"%variable
-        sys.exit()
-    else:
-        if not os.path.isfile(filename):
-            print "%s file %s does not exist"%(variable, filename)
-            sys.exit()
-
-def checkFolder(dir):
-	if not os.path.exists(dir):
-		print "Creating folder ./%s"%(dir)
-		os.makedirs(dir)	
-
+#TODO: merge to other functions
 def segmentSubEntries(srtData):
 	for srtEntry in srtData:
-		fileId="%s%04d"%(options.movielang, int(srtEntry['id']))
+		fileId="%s%04d"%(options.movielang, srtEntry.index)
 
 		segmentAudioFile = "%s/%s.wav"%(options.outdir, fileId)
 		subScriptFile = "%s/%s_sub.txt"%(options.outdir, fileId)
 		dubScriptFile = "%s/%s_dub.txt"%(options.outdir, fileId)
 
-		cutAudioWithAvconv(options.audio, srtEntry['startSecs'], srtEntry['durSecs'], segmentAudioFile)
-		srtEntry['fileName'] = segmentAudioFile
+		cutAudioWithAvconv(options.audiofile, subriptime_to_seconds(srtEntry.start), subriptime_to_seconds(srtEntry.duration), segmentAudioFile)
+		wav_file_name = "%s.wav"%(fileId)
 
 		#write subtitle text to a separate file
 		with open(subScriptFile, 'w') as f:
-			f.write(srtEntry['subtext'])
+			f.write(srtEntry.text_without_tags)
 
 		#transcribe the dubbed audio (different than subtitle)
-		if options.transcribe_dub:
-			dubtext = witAiRecognize(segmentAudioFile, credentials.WIT_AI_KEY)
-			#dubtext = googleSpeechRecognize(segmentAudioFile, credentials.GOOGLE_API_KEY)
-			srtEntry['dubtext'] = dubtext
-			if dubtext:
-				with open(dubScriptFile, 'w') as f:
-					f.write(dubtext)
+		# if options.transcribe_dub:
+		# 	dubtext = witAiRecognize(segmentAudioFile, credentials.WIT_AI_KEY)
+		# 	#dubtext = googleSpeechRecognize(segmentAudioFile, credentials.GOOGLE_API_KEY)
+		# 	srtEntry['dubtext'] = dubtext
+		# 	if dubtext:
+		# 		with open(dubScriptFile, 'w') as f:
+		# 			f.write(dubtext)
 
-
-def cutAudioWithAvconv(audioFilename, start_time, cut_duration, outputAudioFilename):
-	#if output file already exists, delete it
-	if os.path.isfile(outputAudioFilename):
-		os.remove(outputAudioFilename)
-	command = "%s -i %s -ss %s -t %s -ac 1 -ar %s %s"%(AVCONV_LOC, audioFilename, start_time, cut_duration, WAV_BITRATE, outputAudioFilename)
-	process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	output, error = process.communicate()
-
-def checkIfAvconvInstalled():
-	command = "%s"%(AVCONV_LOC)
-	try:
-		process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		output, error = process.communicate()
-	except:
-		print("avconv software not found. Either install it or set the AVCONV_LOC variable to its correct path.")
-		sys.exit()
-
-def srtData2Txt(srtData, filename):
-	with open(filename, 'w') as datafile:
-		for data in srtData:
-			datafile.write("%s\t%s\t%s\t%s\t%s\t%s\n"%(data['id'], data['startSecs'], data['duration'], data['fileName'], data['subtext'], data['dubtext']))
-		datafile.close()
+def checkArgument(argname, isFile=False, isDir=False, createDir=False):
+	if not argname:
+		return False
+	else:
+		if isFile and not os.path.isfile(argname):
+			return False
+		if isDir:
+			if not os.path.isdir(argname):
+				if createDir:
+					print("Creating directory %s"%(argname))
+					os.makedirs(argname)
+				else:
+					return False
+	return True
 
 def witAiRecognize(segment, WIT_AI_KEY):
     r = sr.Recognizer()
@@ -219,34 +131,127 @@ def googleSpeechRecognize(segment, GOOGLE_API_KEY):
 	return result
 
 
+def cutAudioWithPydub(audio_segment, start_time, end_time, outputfile):
+	extract = audio_segment[start_time*1000:end_time*1000]
+	extract.export(outputfile, format=options.audioformat)
+
+def extract_audio_segments(proscript, audio_segment, output_dir, file_prefix=""):
+	'''
+	Cuts each segment and outputs as wav+transcript
+	'''
+	for segment in proscript.segment_list:
+		fileId="%s%04d"%(file_prefix, segment.id)
+
+		segmentAudioFile = "%s/%s_audio.wav"%(output_dir, fileId)
+		subScriptFile = "%s/%s_sub.txt"%(output_dir, fileId)
+		#dubScriptFile = "%s/%s_dub.txt"%(output_dir, fileId)
+
+		#cutAudioWithAvconv(audiofile, segment.start_time, segment.get_duration(), segmentAudioFile)
+		cutAudioWithPydub(audio_segment, segment.start_time, segment.end_time, segmentAudioFile)
+
+		#write subtitle text to a separate file
+		with open(subScriptFile, 'w') as f:
+			f.write(segment.transcript)
+
+def subriptime_to_seconds(srTime):
+	'''
+	Convert SubRipTime object to seconds
+	'''
+	t = datetime.combine(date.min, srTime.to_time()) - datetime.min
+	return t.total_seconds()
+
+def normalize_transcript(transcript):
+	'''
+	All text normalization here
+	'''
+	transcript = re.sub('\n', ' ', transcript)
+	return transcript
+
+def to_proscript(srt_data):
+	proscript = Proscript()
+
+	segment_count = 0
+	first_utterance = True
+
+	for index, srt_entry in enumerate(srt_data):
+		start_time = subriptime_to_seconds(srt_entry.start)
+		end_time = subriptime_to_seconds(srt_entry.end)
+
+		transcript = srt_entry.text_without_tags.strip()
+
+		if not transcript.isspace():
+			if first_utterance:
+				curr_seg = Segment()
+				curr_seg.start_time = start_time
+				curr_seg.end_time = end_time
+				curr_seg.transcript += transcript
+				first_utterance = False
+			elif curr_seg.transcript[-1] in SENTENCE_END_MARKS:
+				if curr_seg.transcript and not curr_seg.transcript.isspace():
+					segment_count += 1
+					curr_seg.id = segment_count
+					curr_seg.transcript = normalize_transcript(curr_seg.transcript)
+					proscript.add_segment(curr_seg)
+					#curr_seg.to_string()
+					#print("----====----")
+				curr_seg = Segment()
+				curr_seg.start_time = start_time
+				curr_seg.end_time = end_time
+				curr_seg.transcript += transcript
+				#print("curr_seg:\n%s"%curr_seg.transcript)
+			else:
+				curr_seg.end_time = subriptime_to_seconds(srt_entry.end)
+				curr_seg.transcript += ' ' + transcript
+				#print("curr_seg:\n%s"%curr_seg.transcript)
+
+		if index == len(srt_data) - 1:
+			if curr_seg.transcript and not curr_seg.transcript.isspace():
+				segment_count += 1
+				curr_seg.id = segment_count
+				curr_seg.transcript = normalize_transcript(transcript)
+				proscript.add_segment(curr_seg)
+				#curr_seg.to_string()
+				#print("----====----")
+	return proscript
+
 def main(options):
-	checkIfAvconvInstalled()
-	checkFile(options.audio,"movie audio")
-	checkFile(options.subfile,"subtitle file")
-	checkFolder(options.outdir)
+	checkArgument(options.audiofile, isFile=True)
+	checkArgument(options.subfile, isFile=True)
+	checkArgument(options.outdir, isDir=True, createDir=True)
 
-	print "Audio: %s\nSubtitles: %s\nLanguage: %s\nTranscription: %s"%(options.audio, options.subfile, options.movielang, options.transcribe_dub)
-	print "Reading subtitles...",
-	srtData = readSrt(options.subfile)
-	srtData = cleanSrtData(srtData)
-	print "done"
+	print("Audio: %s\nSubtitles: %s\nLanguage: %s\nTranscription: %s"%(options.audiofile, options.subfile, options.movielang, options.transcribe_dub))
+	print("Reading subtitles...", end="")
+	#srtData = readSrt(options.subfile)
+	#srtData = cleanSrtData(srtData)
+	srtData = pysrt.open(options.subfile)
+	print("done")
 
-	print "Segmenting subtitle entries...",
-	segmentSubEntries(srtData)
-	print "done."
+	audio = AudioSegment.from_file(options.audiofile, format=options.audioformat)
+
+	movie_proscript = to_proscript(srtData)
+
+	proscript_file = "%s/%s_proscript.csv"%(options.outdir, options.movielang)
+	movie_proscript.segments_to_csv(proscript_file, ['id', 'start_time', 'end_time', 'transcript'], delimiter='|')
+
+	extract_audio_segments(movie_proscript, audio, options.outdir, file_prefix=options.movielang)
+
+	#print("Segmenting subtitle entries...", end="")
+	#segmentSubEntries(srtData)
+	#print("done.")
 	#sentenceData2Txt(sentenceData, "%s/%s_sentenceData.csv"%(options.outdir, options.movielang))
-	srtDataFile = "%s/%s_srtData.csv"%(options.outdir, options.movielang)
-	srtData2Txt(srtData, srtDataFile)
-	print "Srt data written to %s"%srtDataFile
+	#srtDataFile = "%s/%s_srtData.csv"%(options.outdir, options.movielang)
+	#srtData2Txt(srtData, srtDataFile)
+	#print("Srt data written to %s"%srtDataFile)
 
 if __name__ == "__main__":
     usage = "usage: %prog [-s infile] [option]"
     parser = OptionParser(usage=usage)
-    parser.add_option("-a", "--audio", dest="audio", default=None, help="movie audio file to be segmented", type="string")
+    parser.add_option("-a", "--audiofile", dest="audiofile", default=None, help="movie audio file to be segmented", type="string")
     parser.add_option("-s", "--sub", dest="subfile", default=None, help="subtitle file (srt)", type="string")
     parser.add_option("-o", "--output-dir", dest="outdir", default=None, help="Directory to output segments and sentences", type="string")
-    parser.add_option("-l", "--lang", dest="movielang", default=None, help="Language of the movie audio (Three letter ISO 639-2/T code)", type="string")
+    parser.add_option("-l", "--lang", dest="movielang", default="", help="Language of the movie audio (Three letter ISO 639-2/T code)", type="string")
     parser.add_option("-t","--transcribe", dest="transcribe_dub", action="store_true", default=False, help="send dubbed audio segments to wit.ai")
+    parser.add_option("-f", "--audioformat", dest="audioformat", default="wav", help="Audio format (wav, mp3 etc.)", type="string")
 
     (options, args) = parser.parse_args()
 
